eee7a1ac9196918b545e34c03159cd2feafaf188
daniel.falzon <danie.falzon6@gmail.com>
2023-05-14 21:10:20 +0200

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " bg-yellow-100 bg-opacity-70"}>{children}</body>
     </html>
   )
 }
