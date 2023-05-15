f0e2e1419069d98a348e2f027b6c74ec9098c23a
daniel.falzon <danie.falzon6@gmail.com>
2023-05-16 00:01:12 +0200

    Removed borders, started design on skills page

---
+++ src/app/introduction/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import SkillItem from "@/components/skill-item.jsx"
+
+const Page = () => {
+    return ( 
+        <main className="flex min-h-screen flex-col justify-between">
+                <div className='w-full text-center py-2 text-md'>
+                    <Link href="/">Home</Link>
+                </div>
+                <div className="flex-grow p-8 flex flex-col justify-between">
+                    <h1 className="text-7xl flex-grow-2 mb-10">Here's what I do...</h1>
+                    
+                    <div className="flex flex-col text-md flex-grow justify-between">
+                        <SkillItem name="Engineering Management"></SkillItem>
+                        <SkillItem name="Web Development"></SkillItem>
+                        <SkillItem name="Product Management"></SkillItem>
+                        <SkillItem name="Agile Adoption"></SkillItem>
+                        <SkillItem name="Process Optimistation"></SkillItem>
+                    </div>
+                </div>
+
+                <div className='w-full text-center py-2 text-md'>
+                    Toolkit
+                </div>
+        </main>
+     );
+}
+ 
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-8 pb-0">
-      <div className='flex justify-around flex-col border-solid border-2 border-b-0 border-black p-8 flex-grow'>
-        <div>
+    <main className="flex min-h-screen flex-col justify-between pb-0">
+      <div className='relative flex justify-around flex-col  p-8 flex-grow'>
+        <div className='relative'>
           <h1  className='text-7xl flex-grow-2'>Hi, <br /> <span className='text-6xl'>I'm Daniel</span></h1>
           <hr className="my-3 h-0.5 border-t-0  bg-black opacity-0" />
-          <span className='italic text-black text-xl'>Engineering Manager,</span> 
-          <span className='italic text-black text-xl'> FullstackWeb Developer</span>
+          <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] rounded-full text-center mx-auto top-0 right-0"
+          src="/profile.jpeg" 
+          alt="Next.js Logo"
+          width={150}
+          height={180}
+          
+        />
+        <hr className="my-3 h-0.5 border-t-0  bg-black opacity-0" />
+          <div className='italic text-black text-xl text-center'>Engineering Manager,</div> 
+          <div className='italic text-black text-xl text-center'> Fullstack Web Developer</div>
         </div>
-
 
         <div className="flex-grow-2 italic text-xl text-black">
           <span className='text-6xl bold text-black'>"</span>
           <br/>
-          <span> <strong>Serving</strong>, <strong>scaling</strong> and <strong>mentoring</strong> engineering teams. Passionate about web development.</span>
+          <span> <strong>Serving</strong>, <strong>scaling</strong> and <strong>mentoring </strong> 
+          engineering teams. Passionate about web development.</span>
           <br/>
           <span className='text-6xl bold text-black w-full text-right block'>"</span>
         </div>
@@ -26,8 +36,9 @@ export default function Home() {
         </button>
       </div>
 
-      <div className='border-dashed border-x-2 border-black w-full h-full text-center py-2 text-md'>
-        Introduction
+      {/* Scroll indicator component */}
+      <div className='w-full h-full text-center py-2 text-md'>
+        <Link href="/introduction">Skills</Link>
       </div>
 
       {/* <Image
